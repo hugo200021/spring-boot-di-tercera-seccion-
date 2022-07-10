e6a583cd5a1b6087c62774eb85ae76cfb90b982c
hugo200021 <HG212137@alumno.udb.edu.sv>
2022-07-09 21:29:50 -0600

    video 37

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,14 +14,14 @@ import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 public class IndexController {
 
 	
-	private IServicio servicio;
+
 	
 	
 	@Autowired
-	public IndexController(IServicio servicio) {
-		
-		this.servicio = servicio;
-	}
+	
+	private IServicio servicio;
+	
+	
 	@GetMapping({"/", "", "/index"})
 	public String index(Model model) {
 		model.addAttribute("objeto", servicio.operacion());
