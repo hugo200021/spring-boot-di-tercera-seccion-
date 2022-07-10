232afcff082354ae3b254dc2e07ea1b69e99d8fa
hugo200021 <HG212137@alumno.udb.edu.sv>
2022-07-09 21:04:26 -0600

    video 36

--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,13 +1,15 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component
+@Primary
+@Component("miServicioSimple")
 
 public class MiServicio implements IServicio {
 @Override
 	public String operacion() {
-		return "Ejecutando algun proceso importante...";
+		return "Ejecutando algun proceso importante simple...";
 	}
 	
 }
