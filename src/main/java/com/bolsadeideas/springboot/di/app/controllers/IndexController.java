ebef7a2d083909352b1a45db2c6c789738d7c2e6
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-07-10 10:09:40 -0600

    July 10, 2022 10:09 AM

--- src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.bolsadeideas.springboot.di.app.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 public class IndexController {
 
 	@Autowired
+	@Qualifier("miServicioComplejo")
 	private IServicio servicio;
 
 	@GetMapping({ "/", "", "/index" })
