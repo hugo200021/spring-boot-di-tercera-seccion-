6964c147fe0d1ef33838f80632128bd693336ca7
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-07-10 09:30:16 -0600

    July 10, 2022 9:30 AM

--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,7 +1,9 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+@Primary
 @Component("miServicioSimple")
 public class MiServicio implements IServicio {
 
