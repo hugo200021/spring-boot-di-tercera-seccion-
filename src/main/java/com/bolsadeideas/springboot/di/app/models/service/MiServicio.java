bb2267c84fbaaabc4f163cd40a95e99b0eb935fc
Roberto Flores <fq211776@alumno.udb.edu.sv>
2022-07-10 21:51:01 -0600

    July 10, 2022 9:51 PM

--- src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -3,13 +3,15 @@ package com.bolsadeideas.springboot.di.app.models.service;
 // import org.springframework.context.annotation.Primary;
 // import org.springframework.stereotype.Component;
 
+
 //@Primary
 //@Component("miServicioSimple")
-public class MiServicio implements IServicio {
+public class MiServicio implements IServicio{
 
-	@Override
-	public String operacion() {
-		return "ejecutando algún proceso importante simple ...";
+
+@Override
+	public String operacion(){
+		return "Ejecutando algun proceso importante simple...";
 	}
 
 }
