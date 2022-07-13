bbe806afc747fe0910ed7c8741b5b02e9cfe1f93
DavidDriotis <80718370+DavidDriotis@users.noreply.github.com>
2022-07-13 17:21:02 -0600

    - video 43
    43. Ciclo de vida del componente

--- src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -1,9 +1,14 @@
 package com.bolsadeideas.springboot.di.app;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.bolsadeideas.springboot.di.app.models.domain.ItemFactura;
+import com.bolsadeideas.springboot.di.app.models.domain.Producto;
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicioComplejo;
@@ -22,5 +27,31 @@ public class AppConfig {
 		return new MiServicioComplejo();
 		
 	}
+	
+	@Bean("ItemsFactura")
+	public List<ItemFactura> registrarItems(){
+			Producto producto1 = new Producto("Camara sony", 100);
+			Producto producto2 = new Producto("Bicicleta Bianchi aro 26", 200);
+			
+			ItemFactura linea1 = new ItemFactura(producto1, 2);
+			ItemFactura linea2 = new ItemFactura(producto2, 4);
+			
+			return Arrays.asList(linea1, linea2);
+	}
 
+	@Bean("ItemsFacturaOficina")
+	@Primary
+	public List<ItemFactura> registrarItemsOficina(){
+			Producto producto1 = new Producto("Monitor LG LCD 24", 250);
+			Producto producto2 = new Producto("NoteBook Asus", 500);
+			Producto producto3 = new Producto("Impresora HP Multifuncional", 80);
+			Producto producto4 = new Producto("Escritorio Oficina", 300);
+			
+			ItemFactura linea1 = new ItemFactura(producto1, 2);
+			ItemFactura linea2 = new ItemFactura(producto2, 1);
+			ItemFactura linea3 = new ItemFactura(producto3, 1);
+			ItemFactura linea4 = new ItemFactura(producto4, 1);
+			
+			return Arrays.asList(linea1, linea2, linea3, linea4);
+	}
 }
---
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -0,0 +1,59 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Factura {
+
+	@Value("${factura.descripcion}")
+	private String descripcion;
+	
+	@Autowired
+	private Cliente cliente;
+	
+	@Autowired
+	private List<ItemFactura> items;
+	
+	@PostConstruct
+	public void inicializar() {
+		cliente.setNombre(cliente.getNombre().concat(" ").concat("Jose"));
+		descripcion = descripcion.concat(" del cliente: ").concat(cliente.getNombre());
+	}
+	
+	@PreDestroy
+	public void destruir() {
+		System.out.println("Factura destruida: ".concat(descripcion));
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<ItemFactura> getItems() {
+		return items;
+	}
+
+	public void setItems(List<ItemFactura> items) {
+		this.items = items;
+	}
+
+}
